774a950ef319f78d5759ca313ec0ab7c9aa04acf
Paweł Chołda <choldapawel@gmail.com>
2021-01-12 12:40:52 +0100

    work in ContentBox

---
+++ src/components/atoms/ContentBox/ContentBox.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const BackgroundDiv = styled.div`
+    background-color: ${({ theme }) => theme.AdditionalColorSection }
+`;
+
+export default BackgroundDiv
