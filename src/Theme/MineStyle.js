ff03ccbf3b386244869eeac4b010746671eb25bf
Paweł Chołda <choldapawel@gmail.com>
2021-01-12 09:22:34 +0100

    add basic color in mainTheme

--- src/Theme/MineStyle.js
+++ src/Theme/MineStyle.js
@@ -1,4 +1,10 @@
 export const theme = {
-    
+    PaddingOrangeName: "hsl(360, 82%, 17%, 0.6)",
+    PaddingOrangeAboutMe: "hsl(360, 82%, 17%, 0.9)",
+    PaddingOrangeAboutMeBaner: "hsl(360, 82%, 17%, 0.8)",
+    AdditionalColorSection: "hsl(29, 13%, 59%)",
+    BlackPadding:"hsl(0, 0%, 0%, 0.8)",
+    ColorInContactBox: "hsl(0, 82%, 17%)",
+    InputAreaContactBox: "hsl(29, 13%, 59%, 0.21)",
   };
   
