62c10f5b00d5418e0a0437067eb561291381d4f8
Pawciio <choldapawel@gmail.com>
2021-01-21 11:53:40 +0100

    styled organism Contact and atom InputContact

--- src/components/atoms/InputContact/InputContact.js
+++ src/components/atoms/InputContact/InputContact.js
@@ -4,15 +4,29 @@ import styled from 'styled-components';
 const Input = styled.input`
   margin: 1em;
   padding: 0.8em;
-  max-width: 500px;
+  width: 500px;
+  border: none;
+  font-size: 18px;
+  background: ${({ theme }) => theme.colors.InputAreaContactBox};
+  &:focus {
+    outline: 2px solid ${({ theme }) => theme.colors.AdditionalColorSection};
+  }
 `;
 
 const Textarea = styled.textarea`
   margin: 1em;
   padding: 0.8em;
   resize: none;
-  max-width: 500px;
+  width: 500px;
   height: 150px;
+  border: none;
+  font-size: 18px;
+  background: ${({ theme }) => theme.colors.InputAreaContactBox};
+  position: relative;
+
+  &:focus {
+    outline: 2px solid ${({ theme }) => theme.colors.AdditionalColorSection};
+  }
 `;
 
 const InputContact = ({ placeholder, InTexarea }) => (
--- src/components/organisms/Contact/Contact.js
+++ src/components/organisms/Contact/Contact.js
@@ -2,14 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 import BackgroundSection from '../../atoms/BackgroundSection/BackgroundSection';
+import InputContact from '../../atoms/InputContact/InputContact';
 
-const WrapperElement = styled.div`
-  margin: 0;
+const BgContactBox = styled.div`
+  margin: 0.8em;
+  padding: 3.5em;
+  max-width: 750px;
+  background: ${({ theme }) => theme.colors.ColorInContactBox};
+  margin: 0 auto;
+`;
+
+const ContactBox = styled.div`
+  background: white;
+  padding: 2em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Contact = () => (
-  <BackgroundSection Background={''}>
-    <WrapperElement>Hello </WrapperElement>
+  <BackgroundSection Padding="3em 0" Background={''}>
+    <BgContactBox>
+      <ContactBox>
+        <InputContact placeholder="Imię i nazwisko" />
+        <InputContact placeholder="Email" />
+        <InputContact InTexarea placeholder="Wiadomość" />
+      </ContactBox>
+    </BgContactBox>
   </BackgroundSection>
 );
 
