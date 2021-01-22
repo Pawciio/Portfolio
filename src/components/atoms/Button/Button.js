480cf375678e3c7b5ce78d2de58961eb9e847ae2
Pawciio <choldapawel@gmail.com>
2021-01-22 15:49:59 +0100

    added animation to button in project

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -11,6 +11,38 @@ const Button = styled.a`
   margin: 0 1em 1em 0;
   cursor: pointer;
   text-decoration: none;
+  display: inline-block;
+  transition: all 0.3s;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background-color: ${({ BgCol, theme }) => (BgCol ? 'black' : theme.colors.AdditionalColorSection)};
+    transition: all 0.3s;
+    z-index: -1;
+  }
+  &:hover {
+    color: ${({ Color, theme }) => (Color ? 'black' : theme.colors.AdditionalColorSection)};
+    &:before {
+      width: 100%;
+    }
+  }
 `;
 
 export default Button;
