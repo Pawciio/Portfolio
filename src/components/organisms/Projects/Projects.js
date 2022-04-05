df58b38fb43485ce1411974fd834a4408536c57c
Pawciio <choldapawel@gmail.com>
2022-04-05 16:26:07 +0200

    change DataTab to other file .json

--- src/components/organisms/Projects/Projects.js
+++ src/components/organisms/Projects/Projects.js
@@ -8,6 +8,8 @@ import ContentBox from "../../atoms/ContentBox/ContentBox";
 import Paragraph from "../../atoms/Paragraph/Paragraph";
 import Button from "../../atoms/Button/Button";
 
+import DataTab from "./DataProjects.json";
+
 import PaulCoderIcon from "../../../assets/Portfolio/paul.png";
 import iCommunityIcon from "../../../assets/Portfolio/comm.png";
 import PreattyWomanIcon from "../../../assets/Portfolio/prete.png";
@@ -69,82 +71,58 @@ const WrapperButton = styled.div`
   }
 `;
 
-const DataTab = [
-  {
-    key: 0,
-    Title: "Paul Coder",
-    disc: "lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: PaulCoderIcon,
-    hrefVisit: "https://paulcoder.netlify.app/",
-    hrefGithub: "https://github.com/Pawciio/PaulCoder",
-  },
-  {
-    key: 1,
-    Title: "Pretty Woman - Hair salon",
-    disc: "lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: PreattyWomanIcon,
-    hrefVisit: "https://pretty-woman.netlify.app/",
-    hrefGithub: "https://github.com/Pawciio/Paul-VisitingPage",
-  },
-  {
-    key: 2,
-    Title: "I-community",
-    disc: "lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: iCommunityIcon,
-    hrefVisit: "https://i-community.netlify.app/",
-    hrefGithub: "https://github.com/Pawciio/Paul-iCommunity",
-  },
-  {
-    key: 3,
-    Title: "Todo List",
-    disc: "Old Project. lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: ToDoList,
-    hrefVisit: "https://pawciio.github.io/Todo-list",
-    hrefGithub: "https://github.com/Pawciio/Todo-list",
-  },
-  {
-    key: 4,
-    Title: "Cookie Clicker",
-    disc: "Old Project. lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: CookieClicker,
-    hrefVisit: "https://pawciio.github.io/Cookie-clicker",
-    hrefGithub: "https://github.com/Pawciio/Cookie-clicker",
-  },
-  {
-    key: 5,
-    Title: "Rock Paper Scissors",
-    disc: "Old Project. lorem ipsum et ament ble ble itd ble loremowe blee ipsumowo fe fe",
-    image: RockPaperScissors,
-    hrefVisit: "https://pawciio.github.io/Rock_Paper_Scissors",
-    hrefGithub: "https://github.com/Pawciio/Rock_Paper_Scissors",
-  },
-];
+const Projects = () => {
 
-const Projects = () => (
-  <BackgroundSection id="Projects" BgCol="white" Padding="6em 0">
-    <TitleBox>Zrealizowane Projekty</TitleBox>
-    <WrapperProjectsBox>
-      {DataTab.map((item) => (
-        <ContentBox key={item.key} className="positionStyledContent" secondary Bgblack>
-          <AddImgProj src={item.image} />
-          <WrapperElement className="positionStyledElementEven">
-            <Paragraph className="TitleProjects" secondary WhiteCol Padding="1em 0">
-              {item.Title}
-            </Paragraph>
-            <Paragraph WhiteCol>{item.disc}</Paragraph>
-            <WrapperButton className="positionStyledButtonBox">
-              <Button href={item.hrefVisit} target="_blank" Color>
-                Wyświetl stronę
-              </Button>
-              <Button href={item.hrefGithub} target="_blank" BgCol>
-                Kod źródłowy
-              </Button>
-            </WrapperButton>
-          </WrapperElement>
-        </ContentBox>
-      ))}
-    </WrapperProjectsBox>
-  </BackgroundSection>
-);
+  const DataTabToMap = DataTab.DataTab;
 
+  const setImage = (imageFromData) => {
+    switch (imageFromData) {
+
+      case "PaulCoderIcon":
+        return PaulCoderIcon;
+
+      case "iCommunityIcon":
+        return iCommunityIcon;
+
+      case "PreattyWomanIcon":
+        return PreattyWomanIcon;
+
+      case "CookieClicker":
+        return CookieClicker;
+
+      case "ToDoList":
+        return ToDoList;
+
+      case "RockPaperScissors":
+        return RockPaperScissors;
+    }
+  };
+
+  return (
+    <BackgroundSection id="Projects" BgCol="white" Padding="6em 0">
+      <TitleBox>Zrealizowane Projekty</TitleBox>
+      <WrapperProjectsBox>
+        {DataTabToMap.map((item) => (
+          <ContentBox key={item.key} className="positionStyledContent" secondary Bgblack>
+            <AddImgProj src={setImage(item.image)} />
+            <WrapperElement className="positionStyledElementEven">
+              <Paragraph className="TitleProjects" secondary WhiteCol Padding="1em 0">
+                {item.Title}
+              </Paragraph>
+              <Paragraph WhiteCol>{item.disc}</Paragraph>
+              <WrapperButton className="positionStyledButtonBox">
+                <Button href={item.hrefVisit} target="_blank" Color>
+                  Wyświetl stronę
+                </Button>
+                <Button href={item.hrefGithub} target="_blank" BgCol>
+                  Kod źródłowy
+                </Button>
+              </WrapperButton>
+            </WrapperElement>
+          </ContentBox>
+        ))}
+      </WrapperProjectsBox>
+    </BackgroundSection>
+  );
+};
 export default Projects;
